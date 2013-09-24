8a31f5300964d89ac1842aafe78d3b7c4c4424eb
Joe Edelman <joe@citizenlogistics.com>
2013-09-24 01:33:18 -0700

    api works

--- js/background.js
+++ js/background.js
@@ -1,191 +1,213 @@
 var options;
 
+var DB = {
+    add_bout: function(url, json_obj) { localStorage[url] = JSON.stringify(DB.bouts(url).concat([json_obj])); },
+    bouts: function(url) { return JSON.parse(localStorage[url] || "[]"); },
+    set_opinion: function(url, opinion) { localStorage["opinion:" + url] = opinion; },
+    opinion: function(url) { return localStorage["opinion:" + url]; }
+};
+
+
+// TODO: this should be over a 1 week window, not all time
+function total_time_spent(bouts){
+    var sum = 0;
+    for (var i = bouts.length - 1; i >= 0; i--) {
+        sum += (bouts[i].end_time - bouts[i].start_time)
+    }
+    return sum;
+}
+
+function fnGetDomain(url){
+    if (!url) return;
+    var match = url.match(/:\/\/(.[^/]+)/);
+    if (!match) return url;
+    return (match[1]).replace('www.','');
+}
+
+// bouts totaling > 1h and at least 5d ago and within 1wk of one another
+function contains_week_of_pain(bouts){
+    // FIXME: i'm going to cheat for the moment and just add all the bouts
+    var sum = total_time_spent(bouts);
+    if (sum > 1*60*60*1000) return true;
+    else return false;
+}
+
+
 var NoRegrets = {
     currentURL: "",
+    start_time: 0,
 
-    addSomeTime: function(url, t) {},
+    user_focused_on_url: function(url) {
+        url = fnGetDomain(url);
+        if (!url) return;
+        if (NoRegrets.currentURL && NoRegrets.currentURL != url) NoRegrets.user_blurred_on_url();
+        console.log('focusing: ' + url);
+        NoRegrets.currentURL = url;
+        NoRegrets.start_time = new Date();
 
+        if (NoRegrets.should_display_review_for_URL(url)){
+            chrome.tabs.executeScript(null, {file: "js/show_review_bar.js"});
+        } else if (NoRegrets.should_display_past_regret_for_URL(url)) {
+            chrome.tabs.executeScript(null, {file: "js/show_regret_bar.js"});
+        }
+    }, 
 
-    // returns array of URL info
-    last_week_URLs: function() {
-
+    user_blurred_on_url: function(url) {
+        url = url || NoRegrets.currentURL;
+        url = fnGetDomain(url);
+        if (!url) return;
+        console.log('blurring: ' + url);
+        DB.add_bout(url, {
+            start_time: NoRegrets.start_time.getTime(),
+            end_time: (new Date()).getTime()
+        })
     },
 
-    // returns URL info
-    info_for_URL: function() {
-
+    should_display_others_regret_for_URL: function(url) {
+        url = fnGetDomain(url);
+        if (!url) return;
+        return false;
     },
 
-    // returns array of URL info
-    most_regretted_URLs: function() {
-
+    should_display_past_regret_for_URL: function(url) {
+        url = fnGetDomain(url);
+        if (!url) return;
+        var opinion = DB.opinion(url);
+        if (opinion && opinion.would_rather_be) return true;
+        // if (tab.url.indexOf("facebook") != -1) return true;
     },
 
-    // returns array of URL info
-    least_regretted_URLs: function() {
-        
+    should_display_review_for_URL: function(url) {
+        url = fnGetDomain(url);
+        if (!url) return;
+        var opinion = DB.opinion(url);
+        if (opinion) return false;
+        if (contains_week_of_pain(DB.bouts(url))) return true;
+        // if (tab.url.indexOf("buzzfeed") != -1) return true;
+        return false;
+    },
+
+    info_for_URL: function(url) {
+        url = fnGetDomain(url);
+        if (!url) return;
+        var result = { url: url };
+        result.opinion = DB.opinion(url);
+        result.bouts = DB.bouts(url);
+        result.total_time_spent = total_time_spent(result.bouts)
+        return result;
     }
+
+    // returns array of URL info
+    // last_week_URLs: function() {},
+
+    // returns array of URL info
+    // most_regretted_URLs: function() {},
+
+    // returns array of URL info
+    // least_regretted_URLs: function() {},
 };
+
+
+
+// tabs created/loaded/removed
+
+chrome.tabs.onUpdated.addListener(function(tabId,changeInfo,tab){
+    if(tab.url.indexOf("http://") != -1 || tab.url.indexOf("https://") != -1){
+        if(changeInfo.status == 'complete'){
+            NoRegrets.user_focused_on_url(tab.url);
+        }
+    }
+});
+
+chrome.tabs.onCreated.addListener(function(tab){
+    if((tab.url.indexOf("http://") != -1 || tab.url.indexOf("https://") != -1)){
+        NoRegrets.user_focused_on_url(tab.url);
+    }
+});
+
+chrome.tabs.onRemoved.addListener(function(tabId,removeInfo){
+    console.log("tab removed");
+    NoRegrets.user_blurred_on_url();
+});
+
+
+
+// window and page focused/blurred
+
+// ....
+
+
+
+
+
+
+// content load
+// chrome.extension.onRequest.addListener(function(request,sender,sendResponse){
+//     if(request.akce == 'content_load'){
+//         wipstats.everyUrlStopLoadTime(sender.tab.id);
+//     }
+// });
+
+// chrome.windows.onFocusChanged.addListener(function(windowId) {
+//     if(windowId != chrome.windows.WINDOW_ID_NONE){
+//         setTimeout(function(){
+//             control.focusActive = true;
+//         },100);
+//     }else{
+//         control.focusActive = false;
+//     }
+// });
+
+// chrome.tabs.onActivated.addListener(function(activeInfo){
+//     control.focusActive = true;
+// });
 
 
 
 // URL info
-var example_url_info_obj = {
-    blamed_url: "",
-    titles: [],
-    total_time_spent: 3,
-    bouts: [{
-        start_time:3,
-        end_time:3,
-        titles: []
-    }],
-    your_opinion: {
-        was_well_spent: false,
-        well_spent_because: "",
-        rather_be: ""
-    },
-    popular_opinions: [{
-        was_well_spent: false,
-        well_spent_because: "",
-        rather_be: ""
-    }]
-};
+// var example_url_info_obj = {
+//     blamed_url: "",
+//     titles: [],
+//     total_time_spent: 3,
+//     bouts: [{
+//         start_time:3,
+//         end_time:3,
+//         titles: []
+//     }],
+//     your_opinion: {
+//         was_well_spent: false,
+//         well_spent_because: "",
+//         rather_be: ""
+//     },
+//     popular_opinions: [{
+//         was_well_spent: false,
+//         well_spent_because: "",
+//         rather_be: ""
+//     }]
+// };
 
-
-
-function write(){//write today stats in localstorage
-    var date = getDateToday();
-    var visits = getVisits();
-    visits[date] = getVisits(date);
-    setVisits(visits);
-}
-
-function fnGetDomain(url){
-    return (url.match(/:\/\/(.[^/]+)/)[1]).replace('www.','');
-}
 
 ////////////   N E W   S Y S T E M   ///////////
 
-var control = {
-    savePrefActive: true,
-    focusActive: true,
-    tempVisits: {},
-    iconCounter: 0,
-    iconLastDomain: '',
-    init: function(){
-        wips.init();
-        options = getOptions();
-        chrome.browserAction.setBadgeBackgroundColor({color:[0, 100, 0, 210]});
-    },
-    checkActualUrl: function(url,tabId){
-        var domain = get_hostname_from_url(url);
-        if(this.tempVisits[domain]){
-            this.tempVisits[domain] ++;
-        }else{
-            this.tempVisits[domain] = 1;
-        }
-        this.iconCounter++;
-        if(this.iconCounter > 10 || this.iconLastDomain != domain){
-            this.changeIconTime(domain,tabId);
-            this.iconCounter = 0;
-            this.iconLastDomain = domain;
-        }
-    },
-    saveTempProfiles: function(){
-        if(this.savePrefActive){
-            var visits = this.getPrefObject('visits');
-            var date = getDateToday();
-            if(!visits[date]){
-                visits[date] = {};
-            }
-            for(var domain in this.tempVisits){
-                var time = this.tempVisits[domain];
-                if(visits[date][domain]){
-                    visits[date][domain] += time;
-                }else{
-                    visits[date][domain] = time;
-                }
-                this.tempVisits[domain] = 0;
-                if(!checkSiteInCategories(domain)){
-                    addSiteToCategory(domain);
-                }
-            }
-            this.setPrefObject('visits',visits);
-            this.savePrefActive = false;
-        }
-    },
-    changeIconTime: function(domain,tabId){
-        var visits = this.getPrefObject('visits');
-        var date = getDateToday();
-        if(visits[date] && visits[date][domain]){
-            var time = visits[date][domain];
-            var minutes = time/60 - ((time/60)%1);
-            if (minutes<60)
-            {
-                chrome.browserAction.setBadgeText({"text": minutes.toString()+ chrome.i18n.getMessage("m"), "tabId": tabId});
-            } else {
-                if (minutes<60*24)
-                {
-                    var hours=time/3600 - ((time/3600)%1);
-                    chrome.browserAction.setBadgeText({"text": hours.toString()+ chrome.i18n.getMessage("h"), "tabId": tabId});
-                } else {
-                        var days=time/(24*3600) - ((time/(24*3600))%1);
-                        chrome.browserAction.setBadgeText({"text": days.toString()+ chrome.i18n.getMessage("d"), "tabId": tabId});
-                }
-            }
-            chrome.browserAction.setTitle( {title:  chrome.i18n.getMessage("siteTitle")+" "+secondsToHourMin(time), tabId: tabId });
-        }
-    },
-    getPrefObject: function(name){
-        var visitsStr = wips.getPref(name);
-        if(!visitsStr){
-            visitsStr = '{}';
-        }
-        return JSON.parse(visitsStr);
-    },
-    setPrefObject: function(name,data){
-        wips.setPref(name,JSON.stringify(data));
-    }
-}
 
-// POSLUCHACE / INTERVALY
+// window.setInterval(function(){
+//     if(control.focusActive){
+//         chrome.tabs.getSelected(null, function(tab){
+//             if(tab.url.indexOf("http://") != -1 || tab.url.indexOf("https://") != -1){
 
-window.setInterval(function(){
-    if(control.focusActive){
-        chrome.tabs.getSelected(null, function(tab){
-            if(tab.url.indexOf("http://") != -1 || tab.url.indexOf("https://") != -1){
-                var options = getOptions();
-                // chrome.idle.queryState(options.idle_time,function(newState){
-                //     if(newState == 'active'){
-                //         control.savePrefActive = true;
-                //         control.checkActualUrl(tab.url,tab.id);
-                //     }
-                // });
-            }
-        });
-    }
-    //console.log(JSON.stringify(control.tempVisits));
-},1000);
+//                 // var options = getOptions();
+//                 // chrome.idle.queryState(options.idle_time,function(newState){
+//                 //     if(newState == 'active'){
+//                 //         control.savePrefActive = true;
+//                 //         control.checkActualUrl(tab.url,tab.id);
+//                 //     }
+//                 // });
+//             }
+//         });
+//     }
+//     //console.log(JSON.stringify(control.tempVisits));
+// },1000);
 
-window.setInterval(function(){
-    control.saveTempProfiles();
-},10000);
-
-chrome.windows.onFocusChanged.addListener(function(windowId) {
-    if(windowId != chrome.windows.WINDOW_ID_NONE){
-        setTimeout(function(){
-            control.focusActive = true;
-        },100);
-    }else{
-        control.focusActive = false;
-    }
-});
-
-chrome.tabs.onActivated.addListener(function(activeInfo){
-    control.focusActive = true;
-});
-
-// load
-window.addEventListener("load",function(){  
-    control.init();  
-},false);
+// window.setInterval(function(){
+//     control.saveTempProfiles();
+// },10000);
