89b27e710c90335cd6288ece5ec107fd2b1a783b
Aza Raskin <aza@air.local>
2013-12-17 19:27:01 -0800

    Slightly better open animation.

--- inject/show_review_bar.js
+++ inject/show_review_bar.js
@@ -5,22 +5,29 @@ if (!window.has_added_no_regrets_iframe){
   var expanded_height = "400px";
   var iframe = document.createElement('iframe');
   iframe.src = chrome.extension.getURL('inject/review_bar.html?param=foo#fragment');
-  iframe.style.height = initial_height;
+  iframe.style.height = 0;
   iframe.style.width = '100%';
   iframe.style.position = 'fixed';
   iframe.style.top = '0';
   iframe.style.left = '0';
+  iframe.style.border = "none";
   iframe.style.zIndex = '938089'; // Some high value
   document.documentElement.appendChild(iframe);
 
   var bodyStyle = document.body.style;
   var cssTransform = 'transform' in bodyStyle ? 'transform' : 'webkitTransform';
   var cssTransition = 'transition' in bodyStyle ? 'transition' : 'webkitTransition';
-  bodyStyle[cssTransform] = 'translateY(' + initial_height + ')';
+  bodyStyle[cssTransform] = 'translateY(0px)';
+  
+  bodyStyle[cssTransition] = 'all .4s ease-out'
+  iframe.style[cssTransition] = 'all .4s ease-out'
 
-  iframe.addEventListener('mouseover', function() {
-    iframe.style[cssTransition] = 'height 1s';
-    bodyStyle[cssTransition] = 'all 1s';
+  setTimeout(function(){
+    bodyStyle[cssTransform] = 'translateY(' + initial_height + ')';
+    iframe.style.height = initial_height
+  }, 20)
+
+  iframe.addEventListener('click', function() {
     iframe.style.height = expanded_height;
     bodyStyle[cssTransform] = 'translateY(' + expanded_height + ')';
   });
